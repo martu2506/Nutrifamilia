db778f60cfdb6ad48bb62cdafc00720b428f30ff
martu2506 <el-manu2011@hotmail.com.ar>
2026-08-24 10:55:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nutrifamilia-v5.7';
+const CACHE='nutrifamilia-v6.0';
 const SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
