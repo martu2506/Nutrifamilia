1dad4af4951b00d81ac3970ea6ae753480cc086c
martu2506 <el-manu2011@hotmail.com.ar>
2026-08-24 00:40:15 -0300

    Add files via upload
    Actualiza Nutrifamilia V5.8

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE='nutrifamilia-web-v5.6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./evidence_registry.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='nutrifamilia-v5.7';
+const SHELL=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match('./index.html'))));
+ if(e.request.method!=='GET')return;
+ const u=new URL(e.request.url); if(u.origin!==location.origin)return;
+ e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))));
 });
